c5321a9cdabaab02392181ee124aed100ea85d97
Benjamin Hergenroeder <benjamin.hergenroeder@comcast.net>
2024-02-25 02:06:54 -0500

    Added inputs and buttons, need to address state and updating heaps

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,20 @@ import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
-  const maxHeap = [12, 10, 9, 5, 6, 1]
-
-  const maxGraph = formGraph(maxHeap)
+  const [maxHeap, setMaxHeap] = useState([])
+  const [minHeap, setMinHeap] = useState([])
   return (
     <>
      <h1>Heapify</h1>
+      <h2>Max Heap</h2>
+      <input type="text" id='maxHeapInput'/>
+      <button>Add to Max Heap</button>
 
-      {maxGraph}
+      {formGraph(maxHeap)}
+      <h2>Min Heap</h2>
+      <input type="text" id='minHeapInput'/>
+      <button>Add to Min Heap</button>
+      {formGraph(minHeap)}
     </>
   )
 }
