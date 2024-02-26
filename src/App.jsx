f3e7c6ae394d11a3b88f0a92a463606fa5206175
Benjamin Hergenroeder <benjamin.hergenroeder@comcast.net>
2024-02-26 16:45:59 -0500

    Added Array view

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import formGraph from './formGraph'
 import './App.css'
 import MaxHeap from './MaxHeap'
@@ -18,28 +18,34 @@ function App() {
 
   return (
     <>
-     <h1>Heapify</h1>
-      <h2>Max Heap</h2>
-      <div id='maxHeap'>
-        <form onSubmit={(e) => {e.preventDefault(); maxHeap.insert(Number(maxInput))}}> 
-          <input type="text" id='maxHeapInput' onChange={(e) => {setMaxInput(e.target.value)}}/>
-          <button className='addButton' type='submit'>Add to Max Heap</button>
-        </form>
-        <button className='removeButton' onClick={() => {maxHeap.delete()}}>Remove priority element</button>
-      </div>
+      <h1>Heapify</h1>
+        <div className='container'>
+          <h2>Max Heap</h2>
+            <div id='maxHeap'>
+              <form onSubmit={(e) => {e.preventDefault(); maxHeap.insert(Number(maxInput))}}> 
+                <input type="text" id='maxHeapInput' onChange={(e) => {setMaxInput(e.target.value)}}/>
+                <button className='addButton' type='submit'>Add to Max Heap</button>
+              </form>
+              <button className='removeButton' onClick={() => {maxHeap.delete()}}>Remove priority element</button>
+            </div>
 
-      {formGraph(maxList)}
+          {formGraph(maxList)}
+          <p>Max Heap as array: [{maxList.map((el) => `${el}`).join(', ')}]</p>
+        </div>
 
-      <h2>Min Heap</h2>
-      <div id='minHeap'>
-        <form onSubmit={(e) => {e.preventDefault(); minHeap.insert(Number(minInput))}}>
-          <input type="text" id='minHeapInput' onChange={(e) => {setMinInput(e.target.value)}}/>
-          <button className='addButton' type='submit'>Add to Min Heap</button>
-        </form>
-        <button className='removeButton' onClick={() => {minHeap.delete()}}>Remove priority element</button>
-      </div>
+        <div className='container'>
+          <h2>Min Heap</h2>
+            <div id='minHeap'>
+              <form onSubmit={(e) => {e.preventDefault(); minHeap.insert(Number(minInput))}}>
+                <input type="text" id='minHeapInput' onChange={(e) => {setMinInput(e.target.value)}}/>
+                <button className='addButton' type='submit'>Add to Min Heap</button>
+              </form>
+              <button className='removeButton' onClick={() => {minHeap.delete()}}>Remove priority element</button>
+            </div>
 
-      {formGraph(minList)}
+          {formGraph(minList)}
+          <p>Min Heap as array: [{minList.map((el) => `${el}`).join(', ')}]</p>
+        </div>
 
     </>
   )
