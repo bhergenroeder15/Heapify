f445bd56d1e50dd5bfe4fe1eed2c33ee9b6abf51
Benjamin Hergenroeder <benjamin.hergenroeder@comcast.net>
2024-02-26 19:08:31 -0500

    Added info section

--- src/App.jsx
+++ src/App.jsx
@@ -19,6 +19,12 @@ function App() {
   return (
     <>
       <h1>Heapify</h1>
+      <p>
+        A Heap is a complete binary tree data structure that satisfies the heap property: for every node, the value of its children is less than or equal to its own value. Heaps are often used to implement priority queues, where the smallest or largest element is always at the root of the tree. 
+      </p>
+      <p>
+        This application provides a visualization of how heaps are constructed and maintained, both as a graph and as an array. Please enter your desired values into the inputs below to see how your heap would be structured.
+      </p>
         <div className='container'>
           <h2>Max Heap</h2>
             <div id='maxHeap'>
